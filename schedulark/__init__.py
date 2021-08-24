0b5c32d1eb67cd1e38a3f2166fe87532e0a3f8ea
Esteban Echeverry <eecheverry@knowark.com>
2021-08-23 21:43:47 -0500

    Bump version: 0.1.7 → 0.1.8

--- schedulark/__init__.py
+++ schedulark/__init__.py
@@ -6,7 +6,7 @@ from .scheduler import Scheduler
 from .queue import Queue, MemoryQueue, SqlQueue, JsonQueue
 
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
